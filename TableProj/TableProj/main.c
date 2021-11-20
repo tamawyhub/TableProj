0f01779b6318abcc6b2f4eb9c8f252a5b83dbdfc
ahmedtom <tamtamawy333@gmail.com>
2021-11-20 04:00:58 +0200

    created Atmel Studio Project

---
+++ TableProj/TableProj/main.c
@@ -0,0 +1,18 @@
+/*
+ * TableProj.c
+ *
+ * Created: 11/20/2021 3:57:48 AM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
